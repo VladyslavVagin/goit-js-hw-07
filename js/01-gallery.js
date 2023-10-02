92124778d3cba43af869e414435c2a0ced1d55bb
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-02 23:37:16 +0200

    Task-01(remain keyboard)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Create markup of gallery elements 
+const galleryMarkup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`
+).join('');
+
+// Add markup to HTML 
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Make delegation of events 
+galleryContainer.addEventListener('click', onClickImageShow);
+
+function onClickImageShow (evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+// Recieve link for origignal image 
+   const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}">`);
+   instance.show();
+};
+
